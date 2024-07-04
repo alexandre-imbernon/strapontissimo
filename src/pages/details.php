1ac1d02576109ec60514937a84606950f26f08fd
asmaa-dreissia <asmaa.dreissia@laplateforme.io>
2024-07-04 15:14:16 +0200

    commit for sql database and details.php

--- src/pages/details.php
+++ src/pages/details.php
@@ -192,16 +192,16 @@ if (isset($_GET['id_product']) && is_numeric($_GET['id_product'])) {
             <textarea id="comment" name="comment" rows="4" required></textarea><br><br>
             <label for="rating">Note :</label>
             <div class="star-rating">
-                <input type="radio" id="1-stars" name="rating" value="1" />
-                <label for="1-stars" class="star">&#9733;</label>
-                <input type="radio" id="2-stars" name="rating" value="2" />
-                <label for="2-stars" class="star">&#9733;</label>
-                <input type="radio" id="3-stars" name="rating" value="3" />
-                <label for="3-stars" class="star">&#9733;</label>
+                <input type="radio" id="5-stars" name="rating" value="5" />
+                <label for="5-stars" class="star">&#9733;</label>
                 <input type="radio" id="4-stars" name="rating" value="4" />
                 <label for="4-stars" class="star">&#9733;</label>
-                <input type="radio" id="5-star" name="rating" value="5" />
-                <label for="5-star" class="star">&#9733;</label>
+                <input type="radio" id="3-stars" name="rating" value="3" />
+                <label for="3-stars" class="star">&#9733;</label>
+                <input type="radio" id="2-stars" name="rating" value="2" />
+                <label for="2-stars" class="star">&#9733;</label>
+                <input type="radio" id="1-star" name="rating" value="1" />
+                <label for="1-star" class="star">&#9733;</label>
             </div><br><br>
             <button type="submit" name="submit_comment">Soumettre</button>
         </form>
